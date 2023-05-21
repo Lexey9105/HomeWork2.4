696b09d3c6c09e926c45e4d95332a4bc2cdda933
Lexey9105 <umpp120@gmail.com>
2023-05-21 19:34:23 +0300

    Initial Commit

---
+++ src/main/java/pro/sky/LoginPassSky/LoginPassSkyApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.LoginPassSky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginPassSkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginPassSkyApplication.class, args);
+	}
+
+}
